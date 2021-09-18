605cb280eb003a103409162d2adf405d48ad7ab2
Danyyl <danyyl.con@gmail.com>
2021-09-18 15:16:17 -0400

    initial project setup with static middleware

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
 import express from 'express';
 import sharp from 'sharp';
-import checkFile from './checkFile';
+import readFile from './readFile';
 
 const app = express();
 const port = 3000;
+
 const imgDir = `${process.cwd()}/src/images/`;
 const thumbDir = `${process.cwd()}/src/images/thumbs/`;
 
+app.use(express.static('src/images', {extensions: ['jpg', 'jpeg'], redirect: false, maxAge: '1d'}));
 
-app.get('/images', checkFile, async (req, res) => {
-   try{             
-        const imgObj = res.locals.imgObj;   
-        await sharp(`${imgDir}${imgObj.filename}.jpg`)
-                .resize({height: imgObj.height, width: imgObj.width})
-                .toFile(`${thumbDir}${imgObj.thumbFile}`);
+app.get('/', async (req, res) => {
+    const url = req.query;
+    const filename: string = `filename=${url.filename}&height=${url.height}&width=${url.width}.jpg`
+    const height = parseInt(url.height as string);
+    const width = parseInt(url.width as string);
+    const html: string = `<img src="/thumbs/${filename}"/>`;
+    const fileCheck = await readFile(`${thumbDir}${filename}`);
 
-        res.sendFile(`${thumbDir}${imgObj.thumbFile}`);  
-    }catch (err) {
-        res.send(`Sorry there was an error: ${err}`);
-    };
+    if(fileCheck){
+        res.send(html);
+    }else{
+        try{             
+            await sharp(`${imgDir}${url.filename}.jpg`)
+                    .resize(width, height)
+                    .toFile(`${thumbDir}${filename}`);
+            res.send(html);
+        }catch (err) {
+            res.send(`Sorry there was an error: ${err}`);
+        };
+    }
 });
 
 app.listen(port, () => {
