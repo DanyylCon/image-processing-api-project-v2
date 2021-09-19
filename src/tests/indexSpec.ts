44ea8805a5de839246320f2950c52927545274f8
Danyyl <danyyl.con@gmail.com>
2021-09-19 10:50:10 -0400

    remove done() from indexSpec

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,9 +4,9 @@ import app from '../index';
 const request = supertest(app);
 describe('Test endpoint responses', () => {
     
-    it('Expect a 200 response for the root route', async (done) => {
+    it('Expect a 200 response for the root route', async () => {
         const response = await request.get("/");
         expect(response.statusCode).toBe(200);
-        done();
+        
     });
 });
